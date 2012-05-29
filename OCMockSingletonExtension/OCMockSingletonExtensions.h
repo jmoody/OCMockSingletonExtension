de120c0c38213bec1fb0a8814299a4db1303d0ac
Joshua Moody <joshuajmoody@gmail.com>
2012-05-29 14:43:27 +0200

    created directories for Foundation and UIKit and logically separated classes, moved prefix file, tried to adjust the Xcode project, but it uses apples unit testing which i am not familar with

--- OCMockSingletonExtension/OCMockSingletonExtensions.h
+++ OCMockSingletonExtension/OCMockSingletonExtensions.h
@@ -4,4 +4,7 @@
 #import "NSObject+SupersequentImplementation.h"
 #import "NSDate+UnitTests.h"
 #import "NSNotificationCenter+UnitTests.h"
+
+#if TARGET_OS_IPHONE
 #import "UIImage+UnitTests.h"
+#endif
